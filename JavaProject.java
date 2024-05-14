33ffb33a2c04bf7474b43918738290bf69073d5d
Aslı Akın <153763076+azaliyasli@users.noreply.github.com>
2024-05-15 01:14:15 +0300

    Update JavaProject.java
    Added new operation names and operations added to switch-case

--- JavaProject.java
+++ JavaProject.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class JavaProject {
+public class MatrixCalculator {
 
     public static void main (String[] args) {
 
@@ -48,6 +48,7 @@ public class JavaProject {
 
         System.out.println("What do you want to calculate with this matrix? :");
         System.out.println("sum => + / subtraction => - / multiplication => * / Diagonal and Trace => d /Symmetric or asymmetric => s /Orthogonal matrix or not => o ");
+        System.out.println("Periodic matrix or not => p /Idempotent matrix or not => i /Nilpotent matrix or not => n / Involutive matrix or not => v ");
 
         char operation = input.next().charAt(0);
 
@@ -55,6 +56,7 @@ public class JavaProject {
             case '+':  c.sum(Matrix1, Matrix2); break;
             case '-':  System.out.println(c.sub(Matrix1, Matrix2 )); break;
             case '*':  System.out.println(c.mult(Matrix1, Matrix2)); break;
+            case 'd':
             case 's':   
             System.out.println("TRANSPOSE OF MATRIX : ");
             int transposematrix1[][]=new int[n][n];
@@ -67,7 +69,7 @@ public class JavaProject {
             }
             boolean symmetric = true;
             boolean asymmetric = true;
-            symmetric = c.checkSymmetryWithRecursive(Matrix1, transposematrix1, n, 0 , 0, symmetric, asymmetric);
+            //function
             if (asymmetric) {
                 System.out.println("Your matrix is an asymmetric matrix.");
             } else if (symmetric) {
@@ -75,12 +77,13 @@ public class JavaProject {
             } else {
                 System.out.println("Your matrix is neither symmetric nor asymmetric matrix.");
             } break;
-            case 'd':
             case 'o':
+            case 'p':
+            case 'i':
+            case 'n':
+            case 'v':
             default: System.out.println("Invalid operation!");
         }
-
-
 
         System.out.println("Do you want to continue(Yes/No): ");
         control = input.next();
@@ -143,21 +146,6 @@ class operations {
             System.out.println(); 
     }
         return 0;
-}
-    boolean checkSymmetryWithRecursive(int[][] Matrix1, int[][] transpose, int n, int i, int j, boolean symmetric, boolean asymmetric) {
-        if (i == n) {
-        return symmetric;
-        }
-        if (j == n) {
-        return checkSymmetryWithRecursive(Matrix1, transpose, n, i + 1, 0, symmetric, asymmetric);
-        }
-        if (transpose[i][j] != Matrix1 [i][j]) {
-        symmetric = false;
-        }
-        if (transpose[i][j] != -Matrix1[i][j]) {
-        asymmetric = false;
-        }
-        return checkSymmetryWithRecursive(Matrix1, transpose, n, i, j + 1, symmetric, asymmetric);
 }
 
 }
