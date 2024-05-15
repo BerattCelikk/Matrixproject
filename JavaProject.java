db60a2e78a076a6c45e164171833bca2fa994100
Aslı Akın <153763076+azaliyasli@users.noreply.github.com>
2024-05-15 23:25:41 +0300

    Update JavaProject.java
    Added some functions 
    Added choice to some of switch-cases and functions.

--- JavaProject.java
+++ JavaProject.java
@@ -47,8 +47,8 @@ public class MatrixCalculator {
 
 
         System.out.println("What do you want to calculate with this matrix? :");
-        System.out.println("sum => + / subtraction => - / multiplication => * / Diagonal and Trace => d /Symmetric or asymmetric => s /Orthogonal matrix or not => o ");
-        System.out.println("Periodic matrix or not => p /Idempotent matrix or not => i /Nilpotent matrix or not => n / Involutive matrix or not => v ");
+        System.out.println("Sum => + / Subtraction => - / Multiplication => * / Diagonal and Trace => d / Symmetric or Asymmetric => s / Orthogonal matrix or not => o ");
+        System.out.println("Periodic matrix or not => p / Idempotent matrix or not => i / Nilpotent matrix or not => n / Involutive matrix or not => v ");
 
         char operation = input.next().charAt(0);
 
@@ -56,32 +56,41 @@ public class MatrixCalculator {
             case '+':  c.sum(Matrix1, Matrix2); break;
             case '-':  System.out.println(c.sub(Matrix1, Matrix2 )); break;
             case '*':  System.out.println(c.mult(Matrix1, Matrix2)); break;
-            case 'd':
-            case 's':   
-            System.out.println("TRANSPOSE OF MATRIX : ");
-            int transposematrix1[][]=new int[n][n];
-            for(int i = 0; i < n; i++) {
-                for(int j = 0; j < n; j++) {
-                    transposematrix1[j][i] = Matrix1[i][j];
-                    System.out.printf("%3d", Matrix1[j][i]);
-                }
-                System.out.println();
-            }
-            boolean symmetric = true;
-            boolean asymmetric = true;
-            //function
-            if (asymmetric) {
-                System.out.println("Your matrix is an asymmetric matrix.");
-            } else if (symmetric) {
-                System.out.println("Your matrix is a symmetric matrix.");
-            } else {
-                System.out.println("Your matrix is neither symmetric nor asymmetric matrix.");
-            } break;
+            case 'd':  System.out.println("Please enter which matrix you want to use?(1/2): "); 
+                       int choice1 = input.nextInt();
+                       if(choice1 == 1) {
+                        c.diagonalANDtrace(Matrix1);
+                       }
+                       else if(choice1 == 2) {
+                        c.diagonalANDtrace(Matrix2);
+                       } break;
+            case 'i': System.out.println("Please enter which matrix you want to use?(1/2): ");
+                      int choice2 = input.nextInt();
+                      if(choice2 == 1) {
+                        System.out.println(c.isIdempotent(Matrix1));
+                      }
+                      else if(choice2 == 2) {
+                        System.out.println(c.isIdempotent(Matrix2));
+                      } break;
+            case 'n': System.out.println("Please enter which matrix you want to use?(1/2): ");
+                      int choice3 = input.nextInt();
+                      if(choice3 == 1) {
+                      System.out.println(c.isNilpotent(Matrix2));
+                      }
+                      else if(choice3 == 2) {
+                      System.out.println(c.isNilpotent(Matrix2));
+                      } break;
+            case 'v': System.out.println("Please enter which matrix you want to use?(1/2): ");
+                      int choice4 = input.nextInt();
+                      if(choice4 == 1) {
+                      c.isInvolitive(Matrix1);;
+                      }
+                      else if(choice4 == 2) {
+                      c.isInvolitive(Matrix2);
+                      } break;
+            case 'p': break;
+            case 's': 
             case 'o':
-            case 'p':
-            case 'i':
-            case 'n':
-            case 'v':
             default: System.out.println("Invalid operation!");
         }
 
@@ -148,4 +157,156 @@ class operations {
         return 0;
 }
 
+void diagonalANDtrace(int[][] Matrix) {
+    int length = Matrix.length;
+    int diag[]=new int[length];
+    int trace = 0;
+        for(int i=0;i<length;i++){
+            for(int j=0;j<length;j++){
+                if(i==j){
+                    diag[i]=Matrix[i][j];
+                    trace+=Matrix[i][j];
+                }
+            }
+        }
+        System.out.println("DIAGONAL OF MATRIX :");
+        for(int i=0;i<length;i++){
+            System.out.printf("%3d",diag[i]);    
+        }
+        System.out.println();
+        System.out.println("TRACE OF MATRIX = "+trace);
+}
+
+int isIdempotent(int[][] Matrix) {
+    int length = Matrix.length;
+    int idempotent_matrix[][]=new int[length][length];
+    System.out.println("IDEMPOTENT MATRIX : ");
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            int sum=0;
+            for(int k=0;k<length;k++){
+                sum+=Matrix[i][k]*Matrix[k][j];
+            }
+            idempotent_matrix[i][j]= sum;
+        }
+    }
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            System.out.printf("%3d",idempotent_matrix[i][j]);
+        }
+        System.out.println();
+    }
+    boolean isidempotent=true;
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            if(idempotent_matrix[i][j]!=Matrix[i][j]){
+                isidempotent=false;
+                break;
+            }
+        }
+        if(!(isidempotent)){
+            break;
+        }
+    }
+    if(isidempotent){
+        System.out.println("Your matrix is an idempotent matrix");
+    }else{
+        System.out.println("Your matrix is not an idempotent matrix");
+    }
+    return 0;
+}
+
+static int isNilpotent(int[][] Matrix) {
+    System.out.println("enter the power of matrix :");
+    Scanner get = new Scanner(System.in);
+    int length = Matrix.length;
+    int power=get.nextInt();
+    int nilpotent_matrix[][]=new int[length][length];
+    System.out.println("NILPOTENT  MATRIX : ");
+    for(int p=1;p<=power;p++){
+        for(int i=0;i<length;i++){
+            for(int j=0;j<length;j++){
+                int sum=0;
+                for(int k=0;k<length;k++){
+                    
+                    sum+=Matrix[i][k]*Matrix[k][j];
+                }
+                nilpotent_matrix[i][j]+= sum;
+            }
+        }    
+    }
+    
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            System.out.printf("%3d",nilpotent_matrix[i][j]);
+        }
+        System.out.println();
+    }
+    boolean isnilpotent=true;
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            if(nilpotent_matrix[i][j]!=0){
+                isnilpotent=false;
+                break;
+            }
+        }
+        if(!(isnilpotent)){
+            break;
+        }
+    }
+    if(isnilpotent){
+        System.out.println("Your matrix is a nilpotent matrix");
+    }else{
+        System.out.println("Your matrix is not a nilpotent matrix");
+    }
+    return 0;
+}
+
+void isInvolitive(int[][] Matrix) {
+    int length = Matrix.length;
+    int involutive_matrix[][]=new int[length][length];
+    System.out.println("INVOLUTIVE MATRIX : ");
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            int sum=0;
+            for(int k=0;k<length;k++){
+                sum+=Matrix[i][k]*Matrix[k][j];
+            }
+            involutive_matrix[i][j]= sum;
+        }
+    }
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            System.out.printf("%3d",involutive_matrix[i][j]);
+        }
+        System.out.println();
+    }
+    int unitmatrix[][]=new int[100][100];
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            if(i==j){
+                unitmatrix[i][j]=1;
+            }
+        }
+    }
+
+    boolean isinvolutive=true;
+    for(int i=0;i<length;i++){
+        for(int j=0;j<length;j++){
+            if(involutive_matrix[i][j]!=unitmatrix[i][j]){
+                isinvolutive=false;
+                break;
+            }
+        }
+        if(!(isinvolutive)){
+            break;
+        }
+    }
+    if(isinvolutive){
+        System.out.println("Your matrix is an involutive matrix");
+    }else{
+        System.out.println("Your matrix is not an involutive matrix");
+    }
+}
+        
 }
